1b4e6f97791bbcf164e1ca724f2b226b825a88ff
AnielloPiscopo <aniello.piscopo0707@gmail.com>
2023-05-12 12:45:13 +0200

    Finished the exercise of the Mainset class

---
+++ src/org/java/myset/MainSet.java
@@ -0,0 +1,21 @@
+package org.java.myset;
+
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
+public class MainSet {
+	public static void main(String[] args) {
+		Random rnd = new Random();
+		Set<Integer> numsList = new HashSet<>();
+		int numOfNums = 5;
+		
+		for(int i=0 ; i<numOfNums ; i++) {
+			int rndNum = rnd.nextInt(10);
+			
+			numsList.add(rndNum);
+		}
+		
+		System.out.println(numsList);
+	}
+}
