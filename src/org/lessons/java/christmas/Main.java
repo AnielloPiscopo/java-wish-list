d0a3f2d3fd4613abdce45b31b4c4cb14d0a2309e
AnielloPiscopo <aniello.piscopo0707@gmail.com>
2023-05-12 11:52:48 +0200

    Finished the exercise

--- src/org/lessons/java/christmas/Main.java
+++ src/org/lessons/java/christmas/Main.java
@@ -1,7 +1,34 @@
 package org.lessons.java.christmas;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
 		
+		List<String> wishList = new ArrayList<>();
+		int wishListLng = wishList.size();
+		int userChoise = 0;
+		
+		do {
+			System.out.println(wishListLng);
+			System.out.println("Cosa vuoi fare"
+					+ "\n1)Aggiungere regalo nella lista dei desideri;"
+					+ "\n2)Finisci di compilare la lista;");
+			userChoise = in.nextInt();
+			
+			if(userChoise == 1) {
+				System.out.print("Cosa vuoi inserire? ");
+				String gift = in.next();
+				wishList.add(gift);
+				wishListLng++;
+			}
+		}while(userChoise!=2);
+		
+		System.out.println(wishList);
+		
+		in.close();
 	}
 }
