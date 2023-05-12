bfd2a3d71bb4be90dc087f56319f6a3de6b21453
AnielloPiscopo <aniello.piscopo0707@gmail.com>
2023-05-12 14:24:13 +0200

    Added the origin and the bound for the rnd number

--- src/org/java/myset/MainSet.java
+++ src/org/java/myset/MainSet.java
@@ -7,11 +7,13 @@ import java.util.Set;
 public class MainSet {
 	public static void main(String[] args) {
 		Random rnd = new Random();
+		int origin = 2;
+		int bound = 13;
 		Set<Integer> numsList = new HashSet<>();
 		int numOfNums = 5;
 		
 		for(int i=0 ; i<numOfNums ; i++) {
-			int rndNum = rnd.nextInt();
+			int rndNum = rnd.nextInt(origin , bound);
 			
 			numsList.add(rndNum);
 		}
